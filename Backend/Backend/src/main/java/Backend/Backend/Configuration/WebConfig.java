92f7f34c740027035e74cb353996de28a80b72c0
kishan963 <kishsnagrawal963@gmail.com>
2023-12-12 11:56:41 +0530

    Added origins

--- Backend/Backend/src/main/java/Backend/Backend/Configuration/WebConfig.java
+++ Backend/Backend/src/main/java/Backend/Backend/Configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Configure the URL pattern to allow CORS for
-                .allowedOrigins("*") // Add your React frontend URL
+                .allowedOrigins("http://localhost:3000","https://sign-up-login-frontend-kishans-projects-53423b62.vercel.app/") // Add your React frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific methods
                 .allowedHeaders("*")
                 .allowCredentials(true);
